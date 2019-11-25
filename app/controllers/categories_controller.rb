a7584a27c4ed4ab7d349688f69b0839d26530101
Matheus Henrique <matheussilva770@gmail.com>
2019-11-25 15:45:45 -0300

    add i18n category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
     @category.user = current_user
     respond_to do |format|
       if @category.save
-        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_url, notice: t('flash.actions.create.notice', model: @category.model_name.human) }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to categories_url, notice: 'Category was successfully updated.' }
+        format.html { redirect_to categories_url, notice: t('flash.actions.update.notice', model: @category.model_name.human) }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ class CategoriesController < ApplicationController
     authorize @category
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: t('flash.actions.destroy.notice') }
       format.json { head :no_content }
     end
   end
