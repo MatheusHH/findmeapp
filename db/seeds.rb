890f6036673774407578294d6b453291e339000e
Matheus Silva <matheuchenko@gmail.com>
2019-12-11 15:22:45 -0300

    Delete

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(name: 'Matheus', role: 'admin', email: 'matheussilva770@gmail.com', password: 'm2h8p0s5', password_confirmation: 'm2h8p0s5')
